af30fa4f2d3a0301a8ea6b66431a1272001fe04b
FabianMC17 <fabian.maquen@usil.pe>
2023-10-27 13:54:14 -0500

    Plantilla

---
+++ ejerciciosCalificados04/Main.java
@@ -0,0 +1,11 @@
+package ejerciciosCalificados04;
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+
+public class Main {
+    public static void main(String[] args) {
+
+        Estudiantes2023 estudiantes = new Estudiantes2023();
+        estudiantes.listarEstudiantes();
+    }
+}
